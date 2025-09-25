0c4ee5994f26d860ee0714801a7ed4dcf16f34a0
DianaEspinal <dianicee2002@gmail.com>
2025-09-25 12:53:15 -0600

    archivos

--- app/views/home.php
+++ app/views/home.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>home</title>
+    <title>Página Principal</title>
 </head>
 <body>
-    
+    <h1>Bienvenido a mi Blog</h1>
+
+    <nav>
+        <a href="/">Inicio</a> |
+        <a href="/perfil">Perfil</a> |
+        <a href="/lenguaje">Lenguaje Favorito</a> |
+        <a href="/contacto">Contactar</a>
+    </nav>
 </body>
 </html>
